aeae2a44dfb15d81dc2db43185767e4b732371c3
AleMejias <alejandro_12_77@hotmail.com>
2021-05-07 18:43:19 -0300

    Avances en el codigo para envio de correo

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,16 +1,26 @@
 let inputCorreo = document.querySelector('#inputCorreo');
+let btnSubmit = document.querySelector('#btnSubmit');
+let form = document.querySelector("#formulario");
+
 const validarCorreoSuscripcion = (valor) => {
     let correoValido = new RegExp(/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i);
     return correoValido.test(valor);
 };
 
 inputCorreo.addEventListener("input",(e) => {
-/*     console.log(e.target.value);
-    console.log(validarCorreoSuscripcion(e.target.value)) */
+
     if(validarCorreoSuscripcion(e.target.value)){
         inputCorreo.classList.remove("noValido");
         inputCorreo.classList.add("valido");
+        btnSubmit.removeAttribute("disabled");
+        btnSubmit.classList.remove("submitDesactivado");
+        form.addEventListener("submit", () => {
+            let email = `mailto:${e.target.value}`;
+            form.setAttribute("action",email);
+        })
     }else{
+        btnSubmit.setAttribute("disabled","");
+        btnSubmit.classList.add("submitDesactivado");
         inputCorreo.classList.remove("valido");
         inputCorreo.classList.add("noValido");
     }
