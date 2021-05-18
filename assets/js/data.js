014f32b2d4072ea68b117292dd7b086ad7f9dce2
AleMejias <alejandro_12_77@hotmail.com>
2021-05-18 01:48:58 -0300

    Se incluyo arrays de objetos y funcionalidad en la tienda

---
+++ assets/js/data.js
@@ -0,0 +1,26 @@
+const DATA = [
+    {
+        nombre:"iPhone 12 Pro",
+        precio: 999,
+        ruta:"../img/home/iphone12pro-1.jpg",
+        categoria:"Celulares"
+    },
+    {
+        nombre:"iPhone SE",
+        precio: 400,
+        ruta:"../img/home/iphonese-1.jpg",
+        categoria:"Celulares"
+    },
+    {
+        nombre:"iPad Pro",
+        precio: 799,
+        ruta:"../img/home/ipadpro-1.jpg",
+        categoria:"Tablets"
+    },
+    {
+        nombre:"Apple Watch Series 6",
+        precio: 399,
+        ruta:"../img/home/watch-1.jpg",
+        categoria:"Apple Watch"
+    },
+];
--- assets/js/tienda.js
+++ assets/js/tienda.js
@@ -1,11 +1,35 @@
 let contenedorProductos = document.querySelector("#productosContenedor");
 let btnCategorias = document.querySelectorAll(".btn__categorias");
-let cardsProductos = document.querySelectorAll(".productos__contenedor--article");
 let btnCuadricula = document.querySelectorAll(".tipoDeCuadricula");
+
+/* FUNCION AUTO INVOCADA QUE ME PINTARA LAS CARDS CON LOS DATOS TRAIDOS DEL ARRAYS DE PRODUCTOS */
+(() => { 
+    for(const productos of DATA){
+        contenedorProductos.innerHTML += `
+        <article class="productos__contenedor--article cuadriculaMultiple mt-4" data-categorias = "${productos.categoria}">
+            <div class="productos__contenedor--contenedorImg">
+                <img class="productos__contenedor--img" src="${productos.ruta}" alt="">
+            </div>
+            <div class="productos__contenedor--contenedorDescripcion">
+                <h4>${productos.nombre}</h4>
+                <span>$${productos.precio}</span>
+            </div>
+            <div class="productos__contenedor--contenedorBoton">
+                <button>Agregar al carrito</button>
+            </div>
+        </article>
+        `;
+    }
+})();
+let cardsProductos = document.querySelectorAll(".productos__contenedor--article");
 const mostrarCards = (categoria) => {
     cardsProductos.forEach((cards) => {
         let tipoDeCard = cards.getAttribute("data-categorias");
-        if(categoria != tipoDeCard){
+        if(categoria == "Todos"){
+            if(cards.classList.contains("activada")){
+                cards.classList.remove("activada");
+            }
+        }else if(categoria != tipoDeCard){
             cards.classList.add("activada");
         }
         else{
@@ -41,4 +65,21 @@ btnCuadricula.forEach((boton) => {
             }
         });
     });
+});
+/* EVENTO PARA CAMBIO DINAMICO DE IMAGEN AL HACER HOVER */
+let img = document.querySelectorAll(".productos__contenedor--img");
+img.forEach((data) => {
+  //CAMBIO LA IMAGEN AL PASAR EL MOUSE POR ARRIBA
+  data.addEventListener("mouseover",(e) => {
+    let viejaURL = e.target.src;
+    let indice = viejaURL.indexOf("-");
+    let valorIndice = parseInt(viejaURL.slice(indice+1,indice+2))+1;
+    let rutaDefinitiva = viejaURL.split("-",1);
+    rutaDefinitiva = `${rutaDefinitiva}-${valorIndice}.jpg`;
+    data.src = rutaDefinitiva;
+    //VUELVO A COLOCAR LA IMAGEN ANTERIOR
+    data.addEventListener("mouseout",() => {
+      data.src = viejaURL;
+    });
+  });
 });
