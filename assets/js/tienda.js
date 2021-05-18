5049342e7701361072136c7b5fc5e5a446324813
AleMejias <alejandro_12_77@hotmail.com>
2021-05-18 10:50:45 -0300

    Comienzo de funcionalidad ordenamiento mediante select

--- assets/js/tienda.js
+++ assets/js/tienda.js
@@ -2,13 +2,12 @@ let contenedorProductos = document.querySelector("#productosContenedor");
 let btnCategorias = document.querySelectorAll(".btn__categorias");
 let btnCuadricula = document.querySelectorAll(".tipoDeCuadricula");
 
-/* FUNCION AUTO INVOCADA QUE ME PINTARA LAS CARDS CON LOS DATOS TRAIDOS DEL ARRAYS DE PRODUCTOS */
-(() => { 
+const imprimirDatos = () => {
     for(const productos of DATA){
         contenedorProductos.innerHTML += `
         <article class="productos__contenedor--article cuadriculaMultiple mt-4" data-categorias = "${productos.categoria}">
             <div class="productos__contenedor--contenedorImg">
-                <img class="productos__contenedor--img" src="${productos.ruta}" alt="">
+                <img class="productos__contenedor--img" src="${productos.ruta}" alt="${productos.nombre}">
             </div>
             <div class="productos__contenedor--contenedorDescripcion">
                 <h4>${productos.nombre}</h4>
@@ -20,11 +19,18 @@ let btnCuadricula = document.querySelectorAll(".tipoDeCuadricula");
         </article>
         `;
     }
+};
+
+/* FUNCION AUTO INVOCADA QUE ME PINTARA LAS CARDS CON LOS DATOS TRAIDOS DEL ARRAYS DE PRODUCTOS */
+(() => { 
+    imprimirDatos();
 })();
 let cardsProductos = document.querySelectorAll(".productos__contenedor--article");
 const mostrarCards = (categoria) => {
+    console.log(categoria)
     cardsProductos.forEach((cards) => {
         let tipoDeCard = cards.getAttribute("data-categorias");
+        console.warn(tipoDeCard)
         if(categoria == "Todos"){
             if(cards.classList.contains("activada")){
                 cards.classList.remove("activada");
@@ -49,6 +55,7 @@ btnCuadricula.forEach((boton) => {
         let cuadriculaUnica = document.querySelector("#cuadriculaUnica");
         let cuadriculaMultiple = document.querySelector("#cuadriculaMultiple");
         cardsProductos.forEach((card) => {
+            console.log(card);
             switch(seleccion){
                 case "multiple":
                     cuadriculaUnica.classList.remove("cuadriculaActiva");
@@ -56,16 +63,34 @@ btnCuadricula.forEach((boton) => {
                     if(card.classList.contains("cuadriculaUnica")){
                         card.classList.remove("cuadriculaUnica");
                         card.classList.add("cuadriculaMultiple");
+                        console.log("Entro a la opcion 1");
                     }
                     break;
                 default:
-                    cuadriculaUnica.classList.add("cuadriculaActiva");
                     cuadriculaMultiple.classList.remove("cuadriculaActiva");
+                    cuadriculaUnica.classList.add("cuadriculaActiva");
+                    card.classList.remove("cuadriculaMultiple");
                     card.classList.add("cuadriculaUnica");
+                    console.log("Entro a la opcion 2");
             }
         });
     });
 });
+/* ORDENAMIENTO MEDIANTE LA ETIQUETA SELECT */
+const ordenarProductos = (tipoDeOrden,arrProductos) => {
+    console.log(tipoDeOrden)
+    contenedorProductos.innerHTML = " ";
+    if(tipoDeOrden == "menor"){
+        arrProductos.sort((a,b) => a.precio - b.precio);
+    }else if(tipoDeOrden == "mayor"){
+        arrProductos.sort((a,b) => b.precio - a.precio);
+    }
+    imprimirDatos();
+};
+let select = document.querySelector("#ordenamiento");
+select.addEventListener("change",(e) => {
+    ordenarProductos(e.target.value,DATA);
+}); 
 /* EVENTO PARA CAMBIO DINAMICO DE IMAGEN AL HACER HOVER */
 let img = document.querySelectorAll(".productos__contenedor--img");
 img.forEach((data) => {
